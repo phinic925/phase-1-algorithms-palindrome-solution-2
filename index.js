2646623358e787023182b6e0c2ec796d98816af0
phinic925 <phinicnyangate480@gmail.com>
2022-06-16 17:25:34 +0100

    commit tree 4fbb94a9016896724f94a41658d9dfb937b96b9c

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+  //loop thru half of the string
+  for(let i=0; i<len/2; i++)
+  {
+    if (word[i] !== word[len -1 -i]){
+      return false;
+    }
+    else{
+      return true;
+    }
+
+  }
+   word = prompt('Enter a word:')
+  //call the function
+  console.log(isPalindrome(madam));
+  
+
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
+
 */
 
 /*
